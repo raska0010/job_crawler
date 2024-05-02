089fb02e34a6d22a74c2ec5fd8ba9fb0834ddf5d
Robert Skatulla <robertskatulla@Roberts-Air.fritz.box>
2024-05-02 11:16:10 +0200

    feat added func create_ad

--- web_tools.py
+++ web_tools.py
@@ -14,3 +14,13 @@ def open_url(url, method, payload=None, timeout=10):
         print(f'Error {e}')
         print(f'Skipping {url}')
         return
+    
+
+def create_ad(job, ads_list, city, date):
+    ads_dict = {}
+    url_name = job.a.text.strip()  # Use ad text to create URL name.
+    url = job.a['href']  # Link to the ad
+    hyperlink_format = '<a href="{url}" target="_blank">{text}</a>'
+    hyperlink = hyperlink_format.format(url=url, text=url_name)
+    ads_dict = ads_dict.update({'job_description': url, 'ad_url': url, 'city': city, 'date': date.today()})
+    return ads_list.append(ads_dict)
